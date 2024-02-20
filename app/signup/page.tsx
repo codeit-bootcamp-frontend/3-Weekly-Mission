adaa839f1724178481c8b0afdf4ded80040a15a9
clyde <clydehan0401@gmail.com>
2024-02-21 04:32:49 +0900

    feat: 로그인, 회원가입 페이지 구현

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,22 @@
-import GoMainLogo from "../../components/GoMainLogo";
+import Link from "next/link";
+import styles from "./index.module.css";
+import BigGoMainLogo from "../../components/BigGoMainLogo";
+import LoginSocialBox from "../../components/LoginSocialBox";
+import SignupForm from "../../components/SignupForm";
 
 const Signup = () => {
   return (
-    <div>
-      <GoMainLogo />
-      <h1>회원가입 페이지입니다.</h1>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <BigGoMainLogo />
+          <span>
+            이미 회원이신가요? <Link href="/signin">로그인 하기</Link>
+          </span>
+        </div>
+        <SignupForm />
+        <LoginSocialBox />
+      </div>
     </div>
   );
 };
--- components/SignupForm/index.tsx
+++ components/SignupForm/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
@@ -8,42 +9,55 @@ import styles from "./index.module.css";
 import eyeOffIcon from "../../images/eye-off.svg";
 import eyeOnIcon from "../../images/eye-on.svg";
 
-const LoginForm = () => {
+const SignupForm = () => {
   const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
-
   const {
     register,
     handleSubmit,
+    watch,
     formState: { isSubmitting, errors },
     setError,
   } = useForm({ mode: "onBlur" });
+  const password = watch("password");
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const checkEmail = async (email) => {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/check-email",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      }
+    );
+    const data = await response.json();
+
+    if (response.ok && !data.isUsableNickname) {
+      setError("email", {
+        type: "manual",
+        message: "이미 사용 중인 이메일입니다.",
+      });
+    }
+  };
+
   const onSubmit = async (data) => {
-    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-up", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-
     if (response.ok) {
       router.push("/folder");
     } else {
-      alert("이메일 또는 비밀번호를 확인해 주세요.");
-      setError("email", {
-        type: "manual",
-        message: "이메일을 확인해 주세요.",
-      });
-      setError("password", {
-        type: "manual",
-        message: "비밀번호를 확인해 주세요.",
-      });
+      alert("회원가입에 실패했습니다.");
     }
   };
 
@@ -62,6 +76,7 @@ const LoginForm = () => {
             value: /\S+@\S+\.\S+/,
             message: "올바른 이메일 주소가 아닙니다.",
           },
+          onBlur: (e) => checkEmail(e.target.value),
         })}
       />
       {errors.email && (
@@ -72,11 +87,15 @@ const LoginForm = () => {
       <input
         id="password"
         type={showPassword ? "text" : "password"}
-        placeholder="비밀번호를 입력해 주세요."
+        placeholder="영문, 숫자를 조합해 8자 이상으로 입력해 주세요."
         style={errors?.password && { borderColor: "red" }}
         className={errors?.password ? styles.error : ""}
         {...register("password", {
           required: "비밀번호를 입력해 주세요.",
+          pattern: {
+            value: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+            message: "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+          },
         })}
       />
       <button
@@ -96,14 +115,51 @@ const LoginForm = () => {
           {errors.password.message as string}
         </p>
       )}
+
+      <label htmlFor="passwordConfirmation">비밀번호 확인</label>
+      <input
+        id="passwordConfirmation"
+        type={showPassword ? "text" : "password"}
+        placeholder="비밀번호와 일치하는 값을 입력해 주세요."
+        style={errors?.password && { borderColor: "red" }}
+        className={errors?.password ? styles.error : ""}
+        {...register("passwordConfirmation", {
+          required: "비밀번호를 입력해 주세요.",
+          validate: (value) =>
+            value === password || "비밀번호가 일치하지 않아요.",
+        })}
+      />
+      <button
+        type="button"
+        onClick={togglePasswordVisibility}
+        className={styles.eyeIcon}
+      >
+        <Image
+          src={showPassword ? eyeOffIcon : eyeOnIcon}
+          alt="비밀번호 보기"
+          width={16}
+          height={16}
+        />
+      </button>
+      {errors.passwordConfirmation && (
+        <p className={styles.errorMessage}>
+          {errors.passwordConfirmation.message as string}
+        </p>
+      )}
+
       <button
         className={styles.formButton}
         type="submit"
         disabled={isSubmitting}
       >
-        로그인
+        회원가입
       </button>
     </form>
   );
 };
-export default LoginForm;
+
+const SignupFormWithNoSSR = dynamic(() => Promise.resolve(SignupForm), {
+  ssr: false,
+});
+
+export default SignupFormWithNoSSR;
