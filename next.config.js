e80852e859164e2f46d713f8c71840e4191f821c
clyde <clydehan0401@gmail.com>
2024-02-21 12:26:46 +0900

    feat: next.config.js 호스트 전체 허용

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 const nextConfig = {
   images: {
-    domains: [
-      "codeit-frontend.codeit.com",
-      "bootcamp-api.codeit.kr",
-      "reactjs.org",
-      "assets.vercel.com",
-      "tanstack.com",
-      "storybook.js.org",
-      "testing-library.com",
-      "static.cdninstagram.com",
-      "s.pstatic.net",
-      "jasonwatmore.com",
-      "codeit-images.codeit.com",
-      "codeit-front.s3.ap-northeast-2.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
     ],
   },
 };
