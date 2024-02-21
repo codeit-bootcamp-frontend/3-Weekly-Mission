ecb4eb0e5ff9214b3db10241daf9b97c3e9c462d
Crack_Egg <jaemyung123@naver.com>
2024-02-21 20:40:34 +0900

    Rename: 폴더 구조 재설계
    - src 폴더 내 페이지 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import GlobalStyle from "../src/styles/GlobalStyle";
+import GlobalStyle from "../styles/GlobalStyle";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
