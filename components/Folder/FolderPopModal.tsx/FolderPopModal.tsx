87197ec1f71c39d56f81904e853fed462a5de34f
Crack_Egg <jaemyung123@naver.com>
2024-02-22 13:21:47 +0900

    Feat: 모달 추가 기능 구현
    - AddModal 함수 구현
    - 스타일 지정
    - ModalInterface 타입 ReactNode 사용해 생성

---
+++ components/Folder/FolderPopModal.tsx/FolderPopModal.tsx
@@ -0,0 +1,11 @@
+import { ModalInterface } from "@/types/types";
+
+export default function FolderPopModal({ onClick }: { onClick: (m: ModalInterface) => void}) {
+  const handleCreate = (id: string) => {
+    if (id === "folderCreate") {
+      const newModal: ModalInterface = {
+        component: <A
+      }
+    }
+  }
+}
--- types/types.tsx
+++ types/types.tsx
@@ -1,7 +1,14 @@
-export default interface FolderInterface {
+import { ReactNode } from "react";
+
+export interface FolderInterface {
   created_at?: string;
   id?: string;
   link?: { count: number };
   name: string;
   user_id?: number;
+}
+
+export interface ModalInterface {
+  component?: ReactNode;
+  show: boolean;
 }
