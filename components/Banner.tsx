d1693fba8b585bf0b61c70fe0c6eed9fff97d867
chlangus <angusangus11@gmail.com>
2024-02-22 15:29:40 +0900

    fix: Banner컴포넌트 tsx로 변환 및 img Image 태그로 변환

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,17 +1,16 @@
+import Image from "next/image";
 import useGetFolderAsync from "../hooks/useGetFolderAsync";
 import styles from "./Banner.module.css";
 
 export default function Banner() {
-  const data = useGetFolderAsync();
-  
+  const data: any = useGetFolderAsync();
+
   return (
     <section className={styles["banner"]}>
       <div className={styles["banner-wrapper"]}>
-        <img
-          className={styles["banner-img"]}
-          src={data?.owner?.profileImageSource}
-          alt="banner-img"
-        ></img>
+        <div className={styles["banner-img"]}>
+          <Image fill src={data?.owner?.profileImageSource} alt="banner-img" />
+        </div>
         <span className={styles["banner-user-name"]}>@{data?.owner?.name}</span>
       </div>
       <div className={styles["banner-title"]}>{data?.name}</div>
