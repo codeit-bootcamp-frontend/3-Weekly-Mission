1b3591fda8e216177f2f9b2669b688a64260b7c2
Crack_Egg <jaemyung123@naver.com>
2024-02-22 20:10:08 +0900

    Feat: 날짜 계산 유틸 기능 구현
    - calcDate 함수 구현

---
+++ components/common/Card/Card.tsx
@@ -0,0 +1,13 @@
+import { CardInterface, FolderInterface, ModalInterface } from "@/types/types";
+
+export default function Card({
+  card,
+  folderList,
+  onClick,
+}: {
+  card: CardInterface;
+  folderList?: FolderInterface[];
+  onClick?: (m: ModalInterface) => void;
+}) {
+  const;
+}
---
+++ utils/calcDate.tsx
@@ -0,0 +1,32 @@
+export default function calcDate(value: string = "") {
+  const current = new Date();
+  const createdDate = new Date(value);
+  const subCalc = (time: number, condition: number, unit: string) => {
+    if (time < 2) {
+      return `1${unit} ago`;
+    } else if (time < condition) {
+      return `${Math.floor(time)} ${unit}s ago`;
+    }
+    return "";
+  };
+
+  let minute: number = (current.getTime() - +createdDate) / 1000 / 60;
+  let unit = subCalc(minute, 60, "minute");
+  if (unit) return unit;
+
+  let hour = minute / 60;
+  unit = subCalc(hour, 24, "hour");
+  if (unit) return unit;
+
+  let day = hour / 24;
+  unit = subCalc(day, 31, "day");
+  if (unit) return unit;
+
+  let month = day / 31;
+  unit = subCalc(month, 12, "month");
+  if (unit) return unit;
+
+  let year = month / 12;
+  unit = subCalc(year, 100, "year");
+  if (unit) return unit;
+}
