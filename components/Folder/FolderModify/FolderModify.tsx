94e1d9fb61c706bdc4083aa9034bd42b931e88e0
Crack_Egg <jaemyung123@naver.com>
2024-02-22 13:43:11 +0900

    Feat: 모달 삭제 기능 구현
    - DeleteModal 함수 구현

---
+++ components/Folder/FolderModify/FolderModify.tsx
@@ -0,0 +1,11 @@
+import { FolderInterface, ModalInterface } from "@/types/types";
+
+export default function FolderModify({
+  folder,
+  onClick,
+}: {
+  folder: FolderInterface;
+  onClick: (m: ModalInterface) => void;
+}) {
+  const;
+}
---
+++ components/modals/DeleteModal/DeleteModal.style.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2.4rem;
+  padding: 3.2rem 4rem;
+`;
+
+export const Title = styled.h2`
+  color: #373740;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+export const Description = styled.span`
+  color: #9fa6b2;
+  text-align: center;
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 22px; /* 157.143% */
+`;
+
+export const Button = styled.button`
+  width: 28rem;
+  color: white;
+  font-size: 1.6rem;
+  padding: 1.6rem 2rem;
+  border-radius: 0.8rem;
+  font-weight: 600;
+  background-color: var(--color-red);
+`;
