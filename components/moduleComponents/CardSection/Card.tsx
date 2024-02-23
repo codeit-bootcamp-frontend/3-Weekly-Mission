363debf0596d1ec3e7d3ebc51070d26e7b012e7b
jiyong0106 <parkjiy96@naver.com>
2024-02-23 13:26:30 +0900

    rename : util 파일명 수정

--- components/moduleComponents/CardSection/Card.tsx
+++ components/moduleComponents/CardSection/Card.tsx
@@ -1,4 +1,4 @@
-import { createDay, createTime } from "../../../src/utils/shared";
+import { createDay, createTime } from "../../../src/utils/timeUtils";
 import { useState } from "react";
 import useUserLinkData from "../../../hook/useUserLinkData";
 import Kebab from "../../atomicComponents/kebab/Kebab";
