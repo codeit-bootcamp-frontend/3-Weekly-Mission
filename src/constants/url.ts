08b37a040adac41f50f7604062056fd2f3694501
Jnnsu <jspp120@gmail.com>
2024-03-03 04:38:56 +0900

    🔥 Remove: url 불필요 코드 제거

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,14 +1,14 @@
 export const API_BASE_URL = 'https://bootcamp-api.codeit.kr/api/linkbrary/v1';
-const USER_ID = 4;
 
-export const API = {
-  ROOT: `${API_BASE_URL}`,
-  USER_SAMPLE: `${API_BASE_URL}/sample/users`,
-  FOLDER_SAMPLE: `${API_BASE_URL}/sample/folders`,
-  USER: `${API_BASE_URL}/users/`,
-  FOLDER: `${API_BASE_URL}/users/${USER_ID}/folders`,
-  FOLDER_LINK: `${API_BASE_URL}/users/${USER_ID}/links`,
-  SIGN_IN: `${API_BASE_URL}/sign-in`,
-  SIGN_UP: `${API_BASE_URL}/sign-up`,
-  CHECK_EMAIL: `${API_BASE_URL}/check-email`,
-} as const;
+// 수정 필요
+// export const API = {
+//   ROOT: `${API_BASE_URL}`,
+//   USER_SAMPLE: `${API_BASE_URL}/sample/users`,
+//   FOLDER_SAMPLE: `${API_BASE_URL}/sample/folders`,
+//   USER: `${API_BASE_URL}/users/`,
+//   FOLDER: `${API_BASE_URL}/users/${USER_ID}/folders`,
+//   FOLDER_LINK: `${API_BASE_URL}/users/${USER_ID}/links`,
+//   SIGN_IN: `${API_BASE_URL}/sign-in`,
+//   SIGN_UP: `${API_BASE_URL}/sign-up`,
+//   CHECK_EMAIL: `${API_BASE_URL}/check-email`,
+// } as const;
