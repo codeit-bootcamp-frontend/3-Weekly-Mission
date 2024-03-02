c2308745a938e58840dfca1697063aad7ded5c3a
Jnnsu <jspp120@gmail.com>
2024-03-03 05:09:05 +0900

    🎨 Style: Kakao 타입 지정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,53 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+interface ShareMethods {
+  sendDefault: (settings: {
+    objectType: 'feed';
+    content: {
+      title: string;
+      description: string;
+      imageUrl: string;
+      link: {
+        mobileWebUrl: string;
+        webUrl?: string;
+      };
+    };
+    buttons: Array<{
+      title: string;
+      link: {
+        mobileWebUrl: string;
+        webUrl?: string;
+      };
+    }>;
+  }) => void;
+}
+interface AuthMethods {
+  login: (settings: {
+    success: (response: Record<string, unknown>) => void;
+    fail: (error: Record<string, unknown>) => void;
+  }) => void;
+  logout: (callback: () => void) => void;
+}
+
+interface APIMethods {
+  request: (settings: {
+    url: string;
+    success: (response: Record<string, unknown>) => void;
+    fail?: (error: Record<string, unknown>) => void;
+    always?: (response: Record<string, unknown>) => void;
+  }) => void;
+}
+
 declare global {
   interface Window {
-    Kakao: any;
+    Kakao: {
+      init: (apiKey: string | undefined) => void;
+      isInitialized: () => boolean;
+      Share: ShareMethods;
+      Auth: AuthMethods;
+      API: APIMethods;
+    };
   }
 }
 
