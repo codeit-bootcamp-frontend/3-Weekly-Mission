1da4fb3cdd456d1b15a2297418574dcb38a699fc
Jnnsu <jspp120@gmail.com>
2024-03-03 04:35:21 +0900

    🔥 Remove: axios 인터셉터 관련 불필요 코드 제거

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,25 +1,5 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
-import { API_BASE_URL } from '@/constants/url';
-
-export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
-  timeout: 5000,
-});
-
-// 요청 인터셉터 추가
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = Cookies.get('accessToken');
-    if (accessToken) {
-      config.headers['Authorization'] = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+import { axiosInstance } from './axiosInterceptors';
 
 // data fetch 로직 분리
 export const fetchData = async (
