8998d31fe02078828fca85a0b539ef3176a9a1d5
Crack_Egg <jaemyung123@naver.com>
2024-03-04 14:58:10 +0900

    Feat: axios interceptors 기능 설정(미완)

--- api/axiosInstance.tsx
+++ api/axiosInstance.tsx
@@ -2,4 +2,49 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://bootcamp-api.codeit.kr/api/",
+  headers: {
+    "Content-Type": "application/json",
+    withCredentials: true,
+  },
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  if (!config.headers) return config;
+  if (typeof window !== "undefined") {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken && config.headers) {
+      config.headers["Authorization"] = accessToken;
+    }
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.config && error.response && error.response.status === 401) {
+      error.config._retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+      error.config.headers.RefreshToken = `${refreshToken}`;
+      return axios
+        .get(`https://bootcamp-api.codeit.kr/docs/linkbrary/v1/auth/`, {
+          headers: {
+            RefreshToken: `${refreshToken}`,
+            "Content-Type": "application/json",
+            withCredentials: true,
+          },
+        })
+        .then(async (response) => {
+          if (response.status === 200 && response.data.accessToken) {
+            localStorage.setItem("accessToken", response.data.accessToken);
+            const accessToken = localStorage.getItem("accessToken");
+            error.config.headers["Authorization"] = `${accessToken}`;
+            return axiosInstance(error.config);
+          }
+        });
+    }
+    return Promise.reject(error);
+  }
+);
