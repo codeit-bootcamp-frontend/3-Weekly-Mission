454e0b8b14df92b1c5dd1cdfe8c56cf1698501d7
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-03-04 13:41:53 +0900

    feat : axios.interceptors.request.use 사용

--- apis/axios.ts
+++ apis/axios.ts
@@ -4,4 +4,17 @@ const axios = baseAxios.create({
   baseURL: "https://bootcamp-api.codeit.kr/api/",
 });
 
+axios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    config.headers.Authorization = `Bearer ${accessToken}`;
+
+    return config;
+  },
+  (error) => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
 export default axios;
